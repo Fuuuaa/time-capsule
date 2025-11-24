5fc410cfb42eefbd3e39420c80f88067b4a46e04
Fuuuaa <joingsss57@gmail.com>
2025-11-24 22:11:16 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryProvider } from "@/providers/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div className="bg-purple-500 text-white p-10">Tailwind работает!</div>
+  );
+}
